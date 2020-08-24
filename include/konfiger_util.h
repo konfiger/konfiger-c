9bb53f29e89aa88e734f79ea0ec608ddd6929b4a
Adewale Azeez <azeezadewale98@gmail.com>
2020-08-24 23:19:35 +0100

    completes the konfiger_util implementation

--- include/konfiger_util.h
+++ include/konfiger_util.h
@@ -14,7 +14,87 @@
 extern "C" {
 #endif
 
+/** 
+    The inline keyword to optimize the function. In 
+    C89 and C90 the inline keyword semantic is 
+    different from current C standard semantic hence 
+    for compilation targeting C89 or C99 the inline 
+    keyword is ommited.
+*/
+#ifdef __STDC_VERSION__
+    #define __KONFIGER_STDC_VERSION__ __STDC_VERSION__
+#else
+    #ifdef __cplusplus
+        #if __cplusplus > 199711L
+            #define __KONFIGER_STDC_VERSION__ __cplusplus
+        #endif
+    #endif
+#endif
+#ifndef __KONFIGER_STDC_VERSION__
+    #define __KONFIGER_INLINE__ 
+    #define __KONFIGER_LONG_LONG__ long
+    #define __FUNCTION__ "<unknown>"
+#else 
+    #define __KONFIGER_INLINE__ inline
+    #define __KONFIGER_LONG_LONG__ long long
+    #define __FUNCTION__ __func__
+#endif
 
+char __KONFIGER_INLINE__ *escape_string(const char *value, char extra_escape[]) {
+    char *final_value = malloc(sizeof(char) * (strlen(value))); // WARN inefficient
+    unsigned index = 0, index2 = 0, index3 = 0;
+    int es_len = (sizeof(extra_escape) / sizeof(extra_escape[0]));
+    while (value[index++] != '\0') {
+        if (es_len > 0 && extra_escape != NULL) {
+            for (index2 = 0; index2 < es_len; ++index2) {
+                if (value[index-1] == extra_escape[index2]) {
+                    final_value[index3] = '^';
+                    ++index3;
+                    break;
+                }
+            }
+        }
+        final_value[index3] = value[index-1];
+        ++index3;
+    }
+    final_value[index3] = '\0';
+    return final_value;
+}
+
+char __KONFIGER_INLINE__ *un_escape_string(const char *value, char extra_escape[]) {
+    char c;
+    char *final_value = malloc(sizeof(char) * (strlen(value))); // WARN inefficient
+    unsigned index = 0, index2 = 0, index3 = 0, continua = 0;
+    int es_len = (sizeof(extra_escape) / sizeof(extra_escape[0]));
+    while (value[index++] != '\0') {
+        c = value[index-1];
+        if (c == '^') {
+            if (value[index] == '\0') {
+                final_value[index3] = c;
+                break;
+            }
+            if (es_len > 0 && extra_escape != NULL) {
+                continua = 0;
+                for (index2 = 0; index2 < es_len; ++index2) {
+                    if (value[index] == extra_escape[index2]) {
+                        continua = 1;
+                        break;
+                    }
+                }
+                if (continua == 1) {
+                    continue;
+                }
+            }
+            final_value[++index3] = '^';
+            final_value[++index3] = value[index];
+            continue;
+        }
+        final_value[index3] = c;
+        ++index3;
+    }
+    final_value[index3] = '\0';
+    return final_value;
+}
 
 #ifdef __cplusplus
 }
--- test/test_util.c
+++ test/test_util.c
@@ -1,10 +1,33 @@
-/*!gcc {0} -I../include/ -I./include/ -I. -o out.exe; ./out.exe --cester-verbose */
+/*!gcc -ansi -pedantic-errors {0} -I../include/ -I./include/ -I. -o out.exe; ./out.exe */
 
 #include <exotic/cester.h>
 #include <konfiger_util.h>
 
-CESTER_TEST(test_check_escape_and_unescape_seperator, _,
-
+CESTER_BODY(
+static char extra_escape[] = {
+    '~'
+};
 )
 
+CESTER_TEST(test_check_escape_and_unescape_seperator, _,
+    char *actual_str = "\\,Hello~W\n-\t-\torld";
+    char *t1 = escape_string(actual_str, extra_escape);
+    char *t2 = un_escape_string(actual_str, NULL);
+    
+    cester_assert_str_not_equal(actual_str, t1);
+    cester_assert_str_equal(t1, "\\,Hello^~W\n-\t-\torld");
+    cester_assert_str_not_equal(t1, un_escape_string(t1, extra_escape));
+    cester_assert_str_not_equal(actual_str, un_escape_string(t1, NULL));
+    cester_assert_str_equal(un_escape_string(t1, extra_escape), actual_str);
+    
+    cester_assert_str_not_equal(t1, t2);
+    cester_assert_str_equal(t2, "\\,Hello~W\n-\t-\torld");
+    cester_assert_str_not_equal(t2, un_escape_string(t1, NULL));
+    cester_assert_str_equal(actual_str, un_escape_string(t2, NULL));
+    cester_assert_str_equal(un_escape_string(t1, extra_escape), actual_str);
+)
 
+CESTER_OPTIONS(
+    CESTER_VERBOSE();
+    CESTER_MINIMAL();
+)
