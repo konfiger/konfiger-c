fe699df53835a3b8db3c9d0a2d3a3691d5deb8ce
Adewale Azeez <azeezadewale98@gmail.com>
2020-09-02 23:25:03 +0100

    create the konfiger stream frunction signatures and structs

--- include/konfiger_stream.h
+++ include/konfiger_stream.h
@@ -14,7 +14,44 @@
 extern "C" {
 #endif
 
+struct konfiger_entry {
+    char *key;
+    char *value;
+};
 
+struct konfiger_stream {
+    unsigned err_tolerance:1;
+    unsigned is_file:1;
+    unsigned trimming_key:1;
+    unsigned trimming_value:1;
+    char delimeter;
+    char seperator;
+    char continuation_char;
+    char *comment_prefix;
+    void *stream_obj;
+};
+
+typedef struct konfiger_entry KonfigerEntry;
+typedef struct konfiger_stream KonfigerStream;
+
+enum konfiger_stat {
+    KONFIGER_OK,
+    KONFIGER_ERR
+};
+
+enum konfiger_stat          ks_init(KonfigerStream **out);
+unsigned                    ks_is_trimming_key(KonfigerStream *stream);
+void                        ks_set_trimming_key(KonfigerStream *stream, unsigned new_value);
+unsigned                    ks_is_trimming_value(KonfigerStream *stream);
+void                        ks_set_trimming_value(KonfigerStream *stream, unsigned new_value);
+char*                       ks_get_comment_prefix(KonfigerStream *stream);
+void                        ks_set_comment_prefix(KonfigerStream *stream, char *new_value);
+char                        ks_get_continuation_char(KonfigerStream *stream);
+void                        ks_set_continuation_char(KonfigerStream *stream, char new_value);
+unsigned                    ks_is_error_tolerant(KonfigerStream *stream);
+void                        ks_error_tolerance(KonfigerStream *stream, unsigned new_value);
+unsigned                    ks_has_next(KonfigerStream *stream);
+enum konfiger_stat          ks_next(KonfigerStream *stream, KonfigerEntry **out);
 
 #ifdef __cplusplus
 }
